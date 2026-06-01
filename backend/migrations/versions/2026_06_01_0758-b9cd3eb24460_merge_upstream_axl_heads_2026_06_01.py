ae0263efb5935dbdd04bb7414bf6dd28c10a8cd2
Slava Rosin <slavarosin@gmail.com>
2026-06-01 08:00:00 +0000

    chore(migrations): drop unused op/sa imports from merge revision (ruff F401)

--- backend/migrations/versions/2026_06_01_0758-b9cd3eb24460_merge_upstream_axl_heads_2026_06_01.py
+++ backend/migrations/versions/2026_06_01_0758-b9cd3eb24460_merge_upstream_axl_heads_2026_06_01.py
@@ -6,10 +6,6 @@ Revises: 13d979938096, 2d316787b998
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'b9cd3eb24460'
 down_revision: Union[str, None] = ('13d979938096', '2d316787b998')
